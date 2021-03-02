8cb9b6653be3fce410af34e6ab4e5db44463cba6
ShabnamNorouzi <shabnam.norouziamin@fathomhq>
2021-03-02 13:52:58 +1000

    enhancement(main): home page

---
+++ src/components/cards.vue
@@ -0,0 +1,190 @@
+<template>
+    <div class="cards">
+        <input type="radio" id="yellow" name="colors" checked>
+        <label for="yellow"></label>
+        <input type="radio" id="pink" name="colors" checked>
+        <label for="pink"></label>
+        <div class="cards-wrapper">
+            <div class="card">
+                <div class="image"></div>
+                <div class="text">
+                    <h5 class="title with-tag"></h5>
+                    <span class="tag"></span>
+                    <span class="tag"></span>
+                    <span class="tag"></span>
+                    <div class="paragraph color-gray">
+                        <p></p>
+                        <p></p>
+                        <p></p>
+                        <p></p>
+                        <p></p>
+                    </div>
+                    <div class="divider">
+                        <button><span></span></button>
+                    </div>
+                </div>
+            </div>
+            <div class="card">
+                <div class="image"></div>
+                <div class="text">
+                    <h5 class="title with-tag"></h5>
+                    <span class="tag"></span>
+                    <span class="tag"></span>
+                    <span class="tag"></span>
+                    <div class="paragraph color-gray">
+                        <p></p>
+                        <p></p>
+                        <p></p>
+                        <p></p>
+                        <p></p>
+                    </div>
+                    <div class="divider">
+                        <button><span></span></button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+
+
+<style scoped>
+:root {
+    --colorMain: #444;
+    --colorLight: #999;
+    --colorAccent: #ddd;
+}
+
+[type="radio"] {
+    display: none;
+}
+label {
+    display: inline-block;
+    width: 30px;
+    height: 30px;
+    margin: 10px;
+    border-radius: 50%;
+    box-shadow: 0 0 0 2px #f9f9f9, 0 0 0 3px #ddd;
+    cursor: pointer;
+}
+
+[type="radio"]:checked + label {
+    box-shadow: 0 0 0 2px #f9f9f9, 0 0 0 3px var(--colorMain);
+}
+
+#yellow + label {
+    background: #ffd94e;
+}
+
+#yellow:checked ~ .cards-wrapper,
+#yellow:checked + label {
+    --colorMain: #ffd94e;
+    --colorLight: #ffe37e;
+    --colorAccent: #ffedae;
+}
+
+#pink + label {
+    background: #ffa1ad;
+}
+
+#pink:checked ~ .cards-wrapper,
+#pink:checked + label {
+    --colorMain: #ffa1ad;
+    --colorLight: #ffc3ca;
+    --colorAccent: #ffdce1;
+}
+
+.cards-wrapper {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    padding: 30px;
+}
+
+.card {
+    flex: 1;
+    background: #fff;
+    padding: 23px 20px;
+    border-radius: 4px;
+    margin: 10px;
+    position: relative;
+    overflow: hidden;
+    text-align: left;
+    max-width: 260px;
+}
+.card:before {
+    content: "";
+    position: absolute;
+    height: 6px;
+    width: 100%;
+    top: -1px;
+    left: 0;
+    background: var(--colorMain);
+}
+.image {
+    background: var(--colorLight);
+    background: linear-gradient(
+    to bottom right,
+    var(--colorLight),
+    var(--colorAccent)
+    );
+    width: 100%;
+    height: 140px;
+    border-radius: 3px;
+}
+.title {
+    margin: 20px 0;
+    background: var(--colorMain);
+    height: 25px;
+    width: 85%;
+    border-radius: 20px;
+}
+.title.with-tag {
+    margin: 20px 0 10px;
+}
+.tag {
+    width: 45px;
+    height: 10px;
+    display: inline-block;
+    margin-right: 8px;
+    border-radius: 20px;
+    background: var(--colorAccent);
+}
+.divider {
+    border-top: 1px solid #eee;
+    padding-top: 20px;
+}
+button {
+    border-radius: 30px;
+    background: var(--colorMain);
+    border: none;
+    border: 4px solid var(--colorMain);
+    width: 100%;
+    padding: 15px;
+    text-align: center;
+}
+button span {
+    width: 40%;
+    height: 8px;
+    background: #fff;
+    display: inline-block;
+    border-radius: 20px;
+}
+.paragraph {
+    margin: 20px 0;
+}
+.paragraph p {
+    width: 90%;
+    background: #ddd;
+    height: 15px;
+    margin: 0 0 12px;
+    border-radius: 20px;
+}
+.paragraph p:nth-child(2n + 2) {
+    width: 80%;
+}
+.paragraph p:last-child {
+    width: 40%;
+}
+</style>
---
+++ src/components/darkMode.vue
@@ -0,0 +1,112 @@
+<template>
+    <div>
+        <input type="checkbox" id="themeSwitch" name="theme-switch" class="theme-switch__input" /> 
+            <label for="themeSwitch" class="theme-switch__label"> 
+                <span></span> 
+            </label>
+        <main class="darkmode">
+            <h2>Css Custom Properties Dark/Light Mode</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa earum eum dicta eius minus voluptatibus odit, suscipit quidem similique
+                consequuntur commodi nisi enim reiciendis porro consequatur eos blanditiis aspernatur ea.
+            </p>
+        </main>
+    </div>
+</template>
+
+<style scoped>
+ 
+.darkmode {
+    --backgroundLight: rgb(255, 255, 255);
+    --textLight: rgb(0, 0, 0);
+    margin: var(--spacing-xlg);
+    padding: calc(var(--spacing-xlg) * 1.5);
+    border: 1px solid #ccc;
+    border-radius: 0.3rem;
+    box-shadow: 0 3px 4px rgba(0, 0, 0, 0.15);
+}
+
+main {
+    --gradDark: #ecf8fa; 
+    --gradLight: #bbbbbb; 
+    background: linear-gradient(to bottom, var(--gradDark), var(--gradLight)); 
+    padding: var(--spacing-md); 
+    text-align: left;
+}
+.theme-switch__input:checked ~ main, 
+.theme-switch__input:checked ~ label {
+    --text: white;
+}
+.theme-switch__input:checked ~ main {
+    --gradDark: var(--color); 
+    --gradLight: #102128;
+    --darken: 0.8;
+    color: var(--text);
+}
+.theme-switch__input,
+.theme-switch__label {
+    position: absolute;
+    z-index: 1;
+}
+.theme-switch__input {
+    opacity: 0;
+}
+.theme-switch__input:hover + .theme-switch__label, 
+.theme-switch__input:focus + .theme-switch__label {
+    background-color: lightSlateGray;
+}
+.theme-switch__input:hover + .theme-switch__label span::after, 
+.theme-switch__input:focus + .theme-switch__label span::after {
+    background-color: #d4ebf2;
+}
+.theme-switch__label {
+    padding: var(--spacing-md); 
+    margin: var(--spacing-lg) var(--spacing-xxlg); 
+    transition: background-color 200ms ease-in-out; 
+    width: 79px; 
+    height: 30px; 
+    border-radius: 50px; 
+    text-align: center; 
+    background-color: slateGray; 
+    box-shadow: -4px 4px 15px inset rgba(0, 0, 0, 0.4);
+}
+.theme-switch__label::before {
+    right: 100%; 
+    margin-right: var(--spacing-sm); 
+}
+.theme-switch__label::after {
+    left: 100%; 
+    margin-left: var(--spacing-sm); 
+    color: lightSlateGray;
+}
+.theme-switch__label span {
+    position: absolute; 
+    bottom: calc(100% + 10px); 
+    left: 0; 
+    width: 100%;
+}
+.theme-switch__label span::after {
+    position: absolute; 
+    top: calc(10% + 15px); 
+    left: var(--spacing-md); 
+    width: 35px; 
+    height: 35px; 
+    content: ''; 
+    border-radius: 50%; 
+    background-color: lightBlue; 
+    transition: transform 200ms, 
+    background-color 200ms; 
+}
+.theme-switch__input:checked ~ .theme-switch__label {
+    background-color: lightSlateGray;
+}
+.theme-switch__input:checked ~ .theme-switch__label::before {
+    color: lightSlateGray;
+}
+.theme-switch__input:checked ~ .theme-switch__label::after {
+    color: turquoise;
+}
+.theme-switch__input:checked ~ .theme-switch__label span::after {
+    transform: translate3d(var(--spacing-xlg), 0, 0);
+}
+
+</style>
